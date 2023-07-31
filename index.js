80aff27b413490a604313b64b9ac1bedd4320352
YJPuk <133621780+YJPuk@users.noreply.github.com>
2023-07-31 18:31:06 +1000

    logo maker functional

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
+//NPM files import and classes from shapes.js
 const inquirer = require("inquirer");
 const fs = require("fs");
 const { Square, Circle, Triangle } = require("./lib/shapes");
 
+
+// Inquirer prompt function 
 function writeToFile(fileName, questions) {
 
+  //Setting height and width of container
     let svgString = "";
     svgString =
     '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
     svgString += "<g>";
     svgString += `${questions.shape}`;
 
+    // User input to adding properties to svg string
     let shapeType;
     if (questions.shape === "Triangle") {
         shapeType = new Triangle();
         svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${questions.shapeColor}"/>`;
     } else if (questions.shape === "Square") {
         shapeType = new Square();
-        svgString += `<rect x="73" y="40" width="160" height="160" fill="${questions.shapeColor}"/>`;
+        svgString += `<rect x="73" y="40" width="150" height="150" fill="${questions.shapeColor}"/>`;
       } else {
         shapeType = new Circle();
         svgString += `<circle cx="150" cy="115" r="80" fill="${questions.shapeColor}"/>`;
     }
 
+    //Text allignment
     svgString += `<text x="150" y="130" text-anchor="middle" font-size="50" fill="${questions.textColor}">${questions.text}</text>`;
     svgString += "</g>";
     svgString += "</svg>";
 
+    //Generating svg file based on name from user input using fs, no errors display the generated logo console log
     fs.writeFile(fileName, svgString, (err) => {
         err ? console.log(err) : console.log("Generated logo.svg");
       });
 }
 
+//Inquirer prompts of text (3 characters), text color, shape and shape color
 function askQuestions() {
     inquirer
       .prompt([
@@ -59,6 +67,7 @@ function askQuestions() {
           name: "shapeColor",
         },
       ])
+      //if user enters more than 3 characters they will receive this error log
       .then((questions) => {
         if (questions.text.length > 3) {
           console.log("Please enter no more than 3 characters");
